bdd7dc6c4e5194ef53cd8c03f95bb8d638a6e117
Glen <glen.tanyonghao@gmail.com>
2024-04-07 02:39:59 +0800

    Small Changes

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,20 +1,22 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import Card from "./components/card";
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Card>
-        <Text style={styles.textContainer}>Power Consumption</Text>
-      </Card>
-      <Card>
-        <Text style={styles.textContainer}>Estate Overview</Text>
-      </Card>
-      <Card>
-        <Text style={styles.textContainer}>Reward Points</Text>
-      </Card>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Card>
+          <Text style={styles.textContainer}>Power Consumption</Text>
+        </Card>
+        <Card>
+          <Text style={styles.textContainer}>Estate Overview</Text>
+        </Card>
+        <Card>
+          <Text style={styles.textContainer}>Reward Points</Text>
+        </Card>
+      </View>
+    </ScrollView>
   );
 };
 
--- Rewards.js
+++ Rewards.js
@@ -1,18 +1,25 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
 import Card from "./components/card";
 import Vouchers from "./components/vouchers";
 
 const Rewards = () => {
   return (
-    <View style={styles.container}>
-      <Card>
-        <Text style={styles.textContainer}>Reward Points</Text>
-        <Text>You earn 50,000 points this month...</Text>
-      </Card>
-      <Vouchers></Vouchers>
-      <Vouchers></Vouchers>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Card>
+          <Text style={styles.textContainer}>Reward Points</Text>
+          <Text>You earn 50,000 points this month...</Text>
+        </Card>
+        <Vouchers></Vouchers>
+        <Vouchers></Vouchers>
+        <Vouchers></Vouchers>
+        <Vouchers></Vouchers>
+        <Vouchers></Vouchers>
+      </View>
+    </ScrollView>
+    // <ScrollView>
+    // </ScrollView>
   );
 };
 

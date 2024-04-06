9e084ee14ea51662a4f6b63ea8335c56f67a3494
Glen <glen.tanyonghao@gmail.com>
2024-04-06 18:31:25 +0800

    Login Button Update

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import BottomTab from './BottomTab';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import BottomTab from "./BottomTab";
+import LoginScreen from "./LoginScreen";
+import HomeScreen from "./HomeScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTab/>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
+        <Stack.Screen name="HomeScreen" component={HomeScreen}></Stack.Screen>
+      </Stack.Navigator>
+      {/* <BottomTab /> */}
+      {/* <LoginScreen></LoginScreen> */}
     </NavigationContainer>
   );
 }

b329b8f25c6a31e83d1c2f34b036a3b5ff1a810a
Glen <glen.tanyonghao@gmail.com>
2024-04-08 17:32:04 +0800

    Updates

--- Rewards.js
+++ Rewards.js
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     backgroundColor: "whitesmoke",
     alignItems: "center",
     justifyContents: "center",
+    height: 900,
   },
   textContainer: {
     fontSize: 15,
